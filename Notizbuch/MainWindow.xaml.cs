adbf79f47815650e630d63155bcb5bc798528030
fabian2005141 <fabian.l05@icloud.com>
2022-07-17 23:21:13 +0200

    V 1.0
    Die Erste Version von der Notizen app

---
+++ Notizbuch/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+//Diagnose 
+using System.Diagnostics;
+
+
+namespace Notizbuch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static string notizPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Notizen/";
+        //static string notizPath = @"E:\Notizen\";
+        static string[] notizen = { };
+        static string notizname;
+        static string offene_notiz;
+        static bool delitem = false;
+        static bool x = false;
+
+
+
+
+        static void if_file()
+        {
+            if (!File.Exists(notizPath))
+            {
+                Directory.CreateDirectory(notizPath);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Grid_new.Visibility = Visibility.Hidden;
+            Save_msg.Visibility = Visibility.Hidden;
+            if_file();
+            notiz();    
+        }
+
+        public void notiz()
+        {
+            System.IO.DirectoryInfo ParentDirectory = new System.IO.DirectoryInfo(notizPath);
+            listBox.Items.Clear();
+            foreach (System.IO.FileInfo f in ParentDirectory.GetFiles())
+            {
+                Debug.WriteLine("Datei: " + f.Name);
+                string data = f.Name;
+                listBox.Items.Add(data);
+            }
+        }
+
+        // Notizen öffnen
+        public void openNotiz(string open)
+        {
+            NotizBox.Text = "";
+            string[] x = File.ReadAllLines(open);
+            notizname = open;
+            for (int i = 0; i < x.Length; i++)
+            {
+                NotizBox.Text += x[i] +"\n";
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                string item = listBox.SelectedItem.ToString();
+                openNotiz(notizPath + listBox.SelectedItem.ToString());
+                notizname = notizPath +
+                 listBox.SelectedItem.ToString();
+                Save_msg.Visibility = Visibility.Hidden;
+                if (delitem == true)
+                {
+                    delete_item(item);
+                    
+                }
+            }
+        }
+        
+        public void delete_item(string item)
+        {
+            File.Delete(notizPath + item);
+            notiz();
+            delitem = false;
+        }
+        
+        public void save(object sender, RoutedEventArgs e)
+        {
+            string text = NotizBox.Text;
+            Debug.WriteLine(text);
+            File.WriteAllText(notizname, text);
+            Save_msg.Visibility = Visibility.Visible;
+        }
+
+        private void New_Notiz(object sender, RoutedEventArgs e)
+        {
+            Grid_new.Visibility = Visibility.Visible;
+        }
+
+        private void ok_create(object sender, RoutedEventArgs e)
+        {
+            string name = textBox_new.Text;
+            Create(notizPath + name + ".txt", " ");
+            textBox_new.Text = "";
+            listBox.Items.Add(name+".txt");
+            Grid_new.Visibility = Visibility.Hidden;
+        }
+
+        private void abbruch_create(object sender, RoutedEventArgs e)
+        {
+            textBox_new.Text = "";
+            Grid_new.Visibility = Visibility.Hidden;
+        
+        }
+
+        public async Task Create(string filePath, string text)
+        {
+            await File.WriteAllTextAsync(filePath, text);  
+        }
+
+        private void delete(object sender, RoutedEventArgs e)
+        {
+            if (delitem == false)
+            {
+                delitem = true;
+            }else
+            {
+                delitem = false;
+              //  listBox.IsEnabled = true;
+            }
+        }
+        
+        
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Notizen-App
-Wurde in einer Code Jam Sitzung auf dem Discord-Server "Deutscher Entwickler Community" Erstellt
+Wurde in einer Code Jam Sitzung auf dem Discord-Server "Deutscher Entwickler Community" am 16.07.2022 Erstellt
+Discord Link: https://discord.gg/BEYya95Gm8
+|----------------------------------------------- Anleitung ------------------------------------------------|
+|                                           ### Datenschutz ###                                            |
+|   # Notiz Ortner : Alle Notizen werden in der appdata (win + r --> %appdata%) unter Notizen Ereichbar    |
+|   # Es werden Keine aufzeichungen vom gerät oder von statistiken online Hochgeladen                      |
+|   # Das Programm ist vollständig offline                                                                 |
+|----------------------------------------------------------------------------------------------------------|
+|                                             ### Bedinung ###                                             |
+|   # Eigendglich selbsterklärent außer die Lösch Funktion                                                 |
+|   # Löschen : auf den Mülleimer Klicken und dan auf eine Notiz Klicken um sie zu löschen                 |
+|   # Achtung: eine bereits vor dem betätigen des Löschbuttons ausgewählte Notiz kann nicht gelöscht werden|
+|   #          die Notizen dürfen nicht ausgewählt sein um sie dan mit der Löschfunktion zu Löschen        |
+|----------------------------------------------------------------------------------------------------------|
+|                                               ###Lizenz ###                                              |
+|   # Du darfst das Programm nutzen und bearbeiten der Löschfunktion                                       |
+|   # Du darfst dieses Programm öffentlich auf Platformen zeigen                                           |
+|   # Da es Viel arbeit war Bitte ich dich dieses Programm nicht als deines auszugeben                     |
+|----------------------------------------------------------------------------------------------------------|
+
+<----------------------------------------------- Viel Spass ----------------------------------------------->
